025091b938f6ea12203a351994976a08f2b4230d
devgatito <eliasmisaelsotojimenez1@gmail.com>
2025-10-28 12:43:17 -0600

    correcion de fondo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@tailwindcss/vite';
 
+
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwind()],
-  },
+  vite: { plugins: [tailwind()] },
 });
